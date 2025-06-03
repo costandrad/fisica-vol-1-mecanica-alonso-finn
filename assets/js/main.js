6b109a730aaea6524619921ad8b28861617814bf
Igo da Costa Andrade <igoandrade@uft.edu.br>
2025-06-03 17:27:21 -0300

    Organização dos arquivos do website

--- assets/js/main.js
+++ assets/js/main.js
@@ -214,7 +214,7 @@
 
 })();
 
-fetch("https://api.github.com/repos/costandrad/template-solucionario-rmd/commits?per_page=1")
+fetch("https://api.github.com/repos/costandrad/fisica-vol-1-mecanica-alonso-finn/commits?per_page=1")
   .then(response => response.json())
   .then(data => {
     const dataCommit = new Date(data[0].commit.committer.date);
